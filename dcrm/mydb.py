9fb57b437197e6475ae4876e19ada730f8ac8a77
MarkReillyGMIT <markreillyblue@hotmail.com>
2023-04-03 17:58:45 +0700

    Setup Django server

--- dcrm/mydb.py
+++ dcrm/mydb.py
@@ -1,3 +1,15 @@
 import mysql.connector
 
-database = mysql.connector
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password'
+)
+
+#prepare cursor object
+cursorObject = database.cursor()
+
+#Create a database 
+cursorObject.execute("CREATE DATABASE db")
+
+print("All done! Database created")
