c222b91b41dbf6852709d8cc9026cf4958d0d610
MarkReillyGMIT <markreillyblue@hotmail.com>
2023-04-07 17:46:23 +0700

    Registration functionality complete

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -2,8 +2,14 @@
 from django.urls import path
 from . import views
 
+#This file allows you to create urls for each page on the website.
+#Each seperate page in the website should be have a seperate URL.
+
+
 urlpatterns = [
     path('', views.home, name='home'),
     #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
+
